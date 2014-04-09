80016b9ef63960cb65882cd9c78e129d7c75ceed
fkelly <frank.kelly@nokia.com>
2014-04-09 14:39:51 -0400

    1) Adding code in src/main/java for Controller 2) Adding Spring to pom.xml 3) Adding BaseController for testing

---
+++ webapp/src/main/java/com/here/BaseController.java
@@ -0,0 +1,31 @@
+package com.here;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+ 
+@Controller
+@RequestMapping("/welcome")
+public class BaseController {
+ 
+	@RequestMapping(method = RequestMethod.GET)
+	public String welcome(ModelMap model) {
+ 
+		model.addAttribute("message", "Maven Web Project + Spring 3 MVC - welcome()");
+ 
+		//Spring uses InternalResourceViewResolver and return back welcome.jsp
+		return "reply";
+ 
+	}
+ 
+	@RequestMapping(value="/{name}", method = RequestMethod.GET)
+	public String welcomeName(@PathVariable String name, ModelMap model) {
+ 
+		model.addAttribute("message", "Maven Web Project + Spring 3 MVC - " + name);
+		return "reply";
+ 
+	}
+ 
+}
