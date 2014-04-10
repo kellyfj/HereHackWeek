c3d20a2b582fcc0822e0171a8fa97db0cf142114
fkelly <frank.kelly@nokia.com>
2014-04-10 11:52:43 -0400

    Now using later version of Spring plus simpler reply.jsp (no HTML tags)

--- webapp/src/main/java/com/here/BaseController.java
+++ webapp/src/main/java/com/here/BaseController.java
@@ -5,23 +5,18 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
  
 @Controller
-@RequestMapping("/welcome")
+@RequestMapping("/")
 public class BaseController {
  
-	@RequestMapping(method = RequestMethod.GET)
+	@RequestMapping(value="/welcome", method = RequestMethod.GET,  produces = "text/plain;charset=UTF-8")
 	public String welcome(ModelMap model) {
-		model.addAttribute("message", "Maven Web Project + Spring 3 MVC - welcome()");
+		model.addAttribute("message", "Maven Web Project + Spring 3 MVC - welcome() "+System.currentTimeMillis());
  
 		//Spring uses InternalResourceViewResolver and return back reply.jsp
 		return "reply";
 	}
- 
-	@RequestMapping(value="/{name}", method = RequestMethod.GET)
-	public String welcomeName(@PathVariable String name, ModelMap model) {
-		model.addAttribute("message", "Maven Web Project + Spring 3 MVC - " + name + " "+System.currentTimeMillis());
-		return "reply";
-	}
- 
+
 }
