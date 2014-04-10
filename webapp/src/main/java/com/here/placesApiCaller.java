2e78fa93e11e1a29ef98c0709fed68968254318d
Will Fergus <will.fergus@here.com>
2014-04-10 11:32:47 -0400

    added Json parsing, object counting, debug prints, and an access function

--- webapp/src/main/java/com/here/placesApiCaller.java
+++ webapp/src/main/java/com/here/placesApiCaller.java
@@ -1,11 +1,20 @@
 package com.here;
 
+
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.io.InputStream;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.List;
+
+import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.JsonParser;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.type.TypeReference;
 
 
 public class placesApiCaller {
@@ -13,8 +22,11 @@ public class placesApiCaller {
 	private static String baseUrl = "http://places.cit.api.here.com/places/v1/discover/search";
 	private static String appId = "4614bb9a70b0ec424d5d8810004c4b0b";
 	private static String appCode = "TvglnNNykdPcXdt61yCiCA";
+	private static Boolean printFlag = false;
 	
-
+	public static void setPrint(Boolean value){
+		printFlag = value;
+	}
 	
     static String readFromInputStream(InputStream input) throws IOException {
         java.util.Scanner scanner = new java.util.Scanner(input).useDelimiter("\\A");
@@ -33,7 +45,8 @@ public class placesApiCaller {
 				+"?app_id="+appId+"&app_code="+appCode
 				+"&at="+longitude+","+latitude
 				+"&q="+name+"&pretty";
-		
+
+		if(printFlag){ System.out.println("URL = "+url); }
 		return url;
 		
 	}
@@ -45,6 +58,8 @@ public class placesApiCaller {
 		String url = baseUrl 
 				+"?app_id="+appId+"&app_code="+appCode
 				+"&q="+name+"%20"+city+"%20"+locale+"&pretty";
+		
+		if(printFlag){ System.out.println("URL = "+url+"\n"); }
 		return url;
 	}
 
@@ -53,17 +68,53 @@ public class placesApiCaller {
 			URLConnection connection = new URL(url).openConnection();
 			connection.setRequestProperty("accept", "application/json");
             String json = readFromInputStream(connection.getInputStream());
-			return json;		
+
+    		if(printFlag){ System.out.println(json); }
+            return json;
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return "";
 	}
 	
-	public static void main(String[] args){
-		//System.out.println(getUrl("PLACE",1.001,2.002));
+	private static Map<String,Object> parseJsonObjectList(String placeObjectList) 
+			throws JsonParseException, JsonMappingException, IOException{
+		Map<String,Object> map = new HashMap<String,Object>();
+		ObjectMapper mapper = new ObjectMapper();
+		 
+		//convert JSON string to Map
+		map = mapper.readValue(placeObjectList, 
+		new TypeReference<HashMap<String,Object>>(){});
 		
-		System.out.print(getPlaceObjectList("http://places.cit.api.here.com/places/v1/discover/search?app_id=4614bb9a70b0ec424d5d8810004c4b0b&app_code=TvglnNNykdPcXdt61yCiCA&at=52.5044,13.3909&q=Speakers&pretty"));
+		return map;
+	}
+	
+	private static int countResults(Map<String,Object> jsonObject)
+	{
+		Map<String,Object> results = (Map<String,Object>) jsonObject.get("results");
+		List items = (List) results.get("items");
+		return items.size();
+	}
+	
+	public static int howManyExist(String name, double lo, double la)
+	{
+		String url = getUrl(name, lo, la);
+		String json = getPlaceObjectList(url);
+		try {
+			Map<String,Object> jsonObjects = parseJsonObjectList(json);
+			return countResults(jsonObjects);
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();		
+			return -1;
+		}
+	}
+	
+	public static void main(String[] args)
+			throws JsonParseException, JsonMappingException, IOException{
+
+		System.out.println(howManyExist("Speakers",52.5044,13.3909));
 		
 	}
 	
